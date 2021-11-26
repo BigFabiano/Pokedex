1ee3b37dd8a30b67ce0afd3fdd9d1949f886874b
Fabiano <nbscbs@gmail.com>
2021-11-25 21:59:29 -0300

    Pokémon Aleátorio - 2021

---
+++ js/index.js
@@ -0,0 +1,29 @@
+const urlPokemonAleatorio = () => {
+    const numeroSorteado = Math.round(Math.random()* 151) + 1;
+    const urlPokemon = `https://pokeapi.co/api/v2/pokemon/${numeroSorteado}`
+    return urlPokemon;
+
+}
+
+
+
+const atualizaTela = (pokemon) => {
+    const imagem = document.getElementById("foto-pokemon");
+    const nomePokemon = document.getElementById("nome-pokemon");
+
+    imagem.src = pokemon.sprites.other["official-artwork"].front_default;
+       
+    nomePokemon.innerHTML = pokemon.name;
+
+}
+
+const iniciar = async() => {
+    const url = urlPokemonAleatorio();
+    const response = await fetch(url);
+    const result = await response.json();
+    atualizaTela(result);
+    
+};
+
+document.addEventListener('DOMContentLoaded', iniciar);
+
